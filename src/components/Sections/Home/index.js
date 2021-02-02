b4d87b631dffbc25dde4c18b8256eada1cb7f8ac
Denilson <denilsonraimundo17@gmail.com>
2021-02-02 00:42:24 -0300

    :hammer: Create function to determine image url by env

--- src/components/Sections/Home/index.js
+++ src/components/Sections/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { getImageUrl } from './../../../utils/getImageUrl'
+
 import * as S from './styles'
 
 const SectionHome = ({ image, presentationH1, presentationH2 }) => (
   <S.SectionContainer>
-    <S.Image src={`http://localhost:1337${image.url}`} alt={image.alternativeText} />
+    <S.Image src={getImageUrl(image.url)} alt={image.alternativeText} />
     <S.PresentationH1>{presentationH1}</S.PresentationH1>
     <S.PresentationH2>{presentationH2}</S.PresentationH2>
   </S.SectionContainer>
